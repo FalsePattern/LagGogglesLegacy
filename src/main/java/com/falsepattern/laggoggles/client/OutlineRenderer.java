21106114a0334b3330628a350990afb93558a0e7
Ven <tudurap.com@gmail.com>
2024-04-27 23:59:43 +0100

    Made Outline Renderer fancy!

--- src/main/java/com/falsepattern/laggoggles/client/OutlineRenderer.java
+++ src/main/java/com/falsepattern/laggoggles/client/OutlineRenderer.java
@@ -9,31 +9,31 @@
 package com.falsepattern.laggoggles.client;
 
 import com.falsepattern.laggoggles.proxy.ClientProxy;
-import com.falsepattern.lib.compat.ChunkPos;
+import com.falsepattern.lib.util.RenderUtil;
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import lombok.val;
-import org.lwjgl.opengl.GL11;
-
-import net.minecraft.client.Minecraft;
+import lombok.var;
 import net.minecraft.client.entity.EntityPlayerSP;
-import net.minecraft.client.multiplayer.ChunkProviderClient;
 import net.minecraft.client.renderer.WorldRenderer;
-import net.minecraft.world.chunk.EmptyChunk;
-import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderWorldEvent;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
-import cpw.mods.fml.common.FMLCommonHandler;
-import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import org.lwjgl.opengl.GL11;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.WeakHashMap;
 
 public class OutlineRenderer {
-    private static final Map<WorldRenderer, Long> rendererCache = new WeakHashMap<>();
+    private static final Map<WorldRenderer, Long> UPDATED_RENDERERS = new WeakHashMap<>();
+
+    private static final long INITIAL_RENDER_MS = 2000;
+    private static final long EXTRA_RENDER_MS = 500;
+    private static final long MAX_RENDER_MS = 6000;
+
+    private static long lastRenderMs;
+
     public void register() {
         FMLCommonHandler.instance().bus().register(this);
         MinecraftForge.EVENT_BUS.register(this);
@@ -41,38 +41,47 @@ public class OutlineRenderer {
 
     @SubscribeEvent
     public void onRenderWorldPost(RenderWorldEvent.Post event) {
-        boolean doRender = ClientProxy.isRenderUpdates();
-        if (!doRender) {
+        if (!ClientProxy.isRenderUpdates())
             return;
+
+        val renderer = event.renderer;
+        var remainingTimeMs = UPDATED_RENDERERS.get(renderer);
+        if (remainingTimeMs == null) {
+            remainingTimeMs = INITIAL_RENDER_MS;
+        } else {
+            remainingTimeMs += EXTRA_RENDER_MS;
+            if (remainingTimeMs > MAX_RENDER_MS)
+                remainingTimeMs = MAX_RENDER_MS;
         }
-        rendererCache.put(event.renderer, System.currentTimeMillis());
+
+        UPDATED_RENDERERS.put(renderer, remainingTimeMs);
     }
+
     @SubscribeEvent
     public void onJoinWorldEvent(EntityJoinWorldEvent e) {
-        if (!e.world.isRemote) {
+        if (!e.world.isRemote)
             return;
-        }
-        if (!(e.entity instanceof EntityPlayerSP)) {
+        if (!(e.entity instanceof EntityPlayerSP))
             return;
-        }
-        rendererCache.clear();
+
+        UPDATED_RENDERERS.clear();
+        lastRenderMs = System.currentTimeMillis();
     }
+
     @SubscribeEvent
     public void onDraw(RenderWorldLastEvent event) {
-        boolean doRender = ClientProxy.isRenderUpdates();
-        if (!doRender) {
+        if (!ClientProxy.isRenderUpdates())
             return;
-        }
-        val MINECRAFT = Minecraft.getMinecraft();
-        float partialTicks = event.partialTicks;
-        double pX = MINECRAFT.thePlayer.prevPosX + (MINECRAFT.thePlayer.posX - MINECRAFT.thePlayer.prevPosX) * partialTicks;
-        double pY = MINECRAFT.thePlayer.prevPosY + (MINECRAFT.thePlayer.posY - MINECRAFT.thePlayer.prevPosY) * partialTicks;
-        double pZ = MINECRAFT.thePlayer.prevPosZ + (MINECRAFT.thePlayer.posZ - MINECRAFT.thePlayer.prevPosZ) * partialTicks;
+        if (UPDATED_RENDERERS.isEmpty())
+            return;
 
+        val currentTimeMs = System.currentTimeMillis();
+        val deltaTimeMs = currentTimeMs - lastRenderMs;
 
         GL11.glPushAttrib(GL11.GL_ALL_ATTRIB_BITS);
         GL11.glPushMatrix();
-        GL11.glTranslated(-pX, -pY, -pZ);
+
+        RenderUtil.setGLTranslationRelativeToPlayer();
 
         GL11.glDisable(GL11.GL_LIGHTING);
         GL11.glDisable(GL11.GL_TEXTURE_2D);
@@ -82,21 +91,10 @@ public class OutlineRenderer {
         GL11.glHint(GL11.GL_LINE_SMOOTH_HINT, GL11.GL_NICEST);
 
         GL11.glBegin(GL11.GL_LINES);
-
-        val iterator = rendererCache.entrySet().iterator();
-        long time = System.currentTimeMillis();
-        while (iterator.hasNext()) {
-            val entry = iterator.next();
-            val renderer = entry.getKey();
-            val deltaTime = time - entry.getValue();
-            if (deltaTime > 2000) {
-                iterator.remove();
-                rendererCache.remove(renderer);
-                continue;
-            }
-            drawChunk(renderer, deltaTime / 2000.0);
-        }
-
+        UPDATED_RENDERERS.replaceAll((renderer, remainingTimeMs) -> {
+            drawChunk(renderer, remainingTimeMs);
+            return remainingTimeMs - deltaTimeMs;
+        });
         GL11.glEnd();
 
         GL11.glEnable(GL11.GL_LIGHTING);
@@ -104,41 +102,67 @@ public class OutlineRenderer {
         GL11.glDisable(GL11.GL_TEXTURE_2D);
         GL11.glPopMatrix();
         GL11.glPopAttrib();
+
+        UPDATED_RENDERERS.entrySet().removeIf(e -> e.getValue() <= 0);
+        lastRenderMs = currentTimeMs;
     }
 
-    private void drawLine(double x1, double y1, double z1, double x2, double y2, double z2){
+    private static void drawChunk(WorldRenderer renderer, long remainingTimeUs) {
+        setColorGradient(remainingTimeUs);
+        drawAABB(renderer);
+    }
+
+    private static void setColorGradient(long remainingTimeUs) {
+        // Shift from red to orange between 6000ms and 4000ms
+        if (remainingTimeUs > 4000) {
+            val delta = (remainingTimeUs - 4000L) / 2000F;
+            GL11.glColor4f(1F, 1 - delta, 0F, 1F);
+            return;
+        }
+
+        // Shift from orange to green between 4000ms and 2000ms
+        if (remainingTimeUs > 2000) {
+            val delta = (remainingTimeUs - 2000L) / 2000F;
+            GL11.glColor4f(delta, 1F, 0F, 1F);
+            return;
+        }
+
+        // Fade out green based on alpha between 2000ms and 0ms
+        val delta = remainingTimeUs / 2000F;
+        GL11.glColor4f(0F, 1F, 0F, delta);
+    }
+
+    private static void drawAABB(WorldRenderer renderer) {
+        // Pushes one block into the chunk
+        val minX = renderer.posX + 1;
+        val minY = renderer.posY + 1;
+        val minZ = renderer.posZ + 1;
+
+        val maxX = minX + 14;
+        val maxY = minY + 14;
+        val maxZ = minZ + 14;
+
+        // Draw Bottom
+        drawLine(minX, minY, minZ, maxX, minY, minZ);
+        drawLine(maxX, minY, minZ, maxX, minY, maxZ);
+        drawLine(maxX, minY, maxZ, minX, minY, maxZ);
+        drawLine(minX, minY, maxZ, minX, minY, minZ);
+
+        // Draw Top
+        drawLine(minX, maxY, minZ, maxX, maxY, minZ);
+        drawLine(maxX, maxY, minZ, maxX, maxY, maxZ);
+        drawLine(maxX, maxY, maxZ, minX, maxY, maxZ);
+        drawLine(minX, maxY, maxZ, minX, maxY, minZ);
+
+        // Draw Vertical
+        drawLine(minX, minY, minZ, minX, maxY, minZ);
+        drawLine(maxX, minY, minZ, maxX, maxY, minZ);
+        drawLine(maxX, minY, maxZ, maxX, maxY, maxZ);
+        drawLine(minX, minY, maxZ, minX, maxY, maxZ);
+    }
+
+    private static void drawLine(double x1, double y1, double z1, double x2, double y2, double z2) {
         GL11.glVertex3d(x1, y1, z1);
         GL11.glVertex3d(x2, y2, z2);
     }
-
-    private void drawChunk(WorldRenderer renderer, double deltaTime) {
-        double a = 1 - deltaTime;
-
-        GL11.glColor4d(0, 1, 0, a);
-
-        int xStart = renderer.posX + 1;
-        int yStart = renderer.posY + 1;
-        int zStart = renderer.posZ + 1;
-        int xEnd = xStart + 14;
-        int yEnd = yStart + 14;
-        int zEnd = zStart + 14;
-
-        //Draw cube wireframe outline
-        //Bottom
-        drawLine(xStart, yStart, zStart, xEnd, yStart, zStart);
-        drawLine(xEnd, yStart, zStart, xEnd, yStart, zEnd);
-        drawLine(xEnd, yStart, zEnd, xStart, yStart, zEnd);
-        drawLine(xStart, yStart, zEnd, xStart, yStart, zStart);
-        //Top
-        drawLine(xStart, yEnd, zStart, xEnd, yEnd, zStart);
-        drawLine(xEnd, yEnd, zStart, xEnd, yEnd, zEnd);
-        drawLine(xEnd, yEnd, zEnd, xStart, yEnd, zEnd);
-        drawLine(xStart, yEnd, zEnd, xStart, yEnd, zStart);
-        //Verticals
-        drawLine(xStart, yStart, zStart, xStart, yEnd, zStart);
-        drawLine(xEnd, yStart, zStart, xEnd, yEnd, zStart);
-        drawLine(xEnd, yStart, zEnd, xEnd, yEnd, zEnd);
-        drawLine(xStart, yStart, zEnd, xStart, yEnd, zEnd);
-    }
-
 }
