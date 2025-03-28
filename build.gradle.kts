76c2ec0e21b49bcb7d41bfc501a25a007c5cae75
FalsePattern <me@falsepattern.com>
2025-03-28 11:36:34 +0100

    update buildscript

---
+++ build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    id("fpgradle-minecraft") version ("0.11.0")
+}
+
+group = "com.falsepattern"
+
+minecraft_fp {
+    mod {
+        modid = "laggoggles"
+        name = "LagGoggles"
+        rootPkg = "$group.laggoggles"
+    }
+    mixin {
+        pkg = "mixin.mixins"
+        pluginClass = "mixin.plugin.MixinPlugin"
+    }
+
+    core {
+        accessTransformerFile = "laggoggles_at.cfg"
+    }
+
+    tokens {
+        tokenClass = "Tags"
+        modid = "MODID"
+        name = "MODNAME"
+        version = "VERSION"
+        rootPkg = "GROUPNAME"
+    }
+
+    publish {
+        changelog = "https://github.com/myname/mymod/releases/tag/$version"
+        maven {
+            repoUrl = "https://mvn.falsepattern.com/releases"
+            repoName = "mavenpattern"
+        }
+        curseforge {
+            projectId = "886297"
+            dependencies {
+                required("fplib")
+            }
+        }
+        modrinth {
+            projectId = "yNnilXec"
+            dependencies {
+                required("fplib")
+            }
+        }
+    }
+}
+
+repositories {
+    cursemavenEX()
+    exclusive(mavenpattern(), "com.falsepattern")
+}
+
+dependencies {
+    implementation("com.falsepattern:falsepatternlib-mc1.7.10:1.2.4:dev")
+    compileOnly(deobfCurse("dragonapi-235591:4722480"))
+}
